6da0935f4c5e8a05352b3b776317ab9e698dd66a
THARUN ANAND <103479706+THARUN1526@users.noreply.github.com>
2023-12-28 21:26:04 +0530

    Add files via upload

---
+++ python7.py
@@ -0,0 +1,7 @@
+figure = px.scatter(data_frame = data, 
+                    x="Notifications",
+                    y="Usage", 
+                    size="Notifications", 
+                    trendline="ols", 
+                    title = "Relationship Between Number of Notifications and Usage")
+figure.show()
